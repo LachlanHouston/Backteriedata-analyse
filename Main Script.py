7d33411f60c4f02f4fc69ca255b5b7ab6d7d99e3
Lachlan Houston <LachlanHouston111@gmail.com>
2021-10-27 16:12:54 +0200

    Add files via upload

--- Main Script.py
+++ Main Script.py
@@ -4,4 +4,27 @@ Created on Wed Oct 27 16:04:58 2021
 
 @author: Lachlan Houston(s214593) og Frederik Ravnborg(sxxxxxx)
 """
+import math
+import numpy as np
+import panda as pd
 
+# 1: Data load Funktion:
+def dataLoad(filename):
+    # Insert your code here
+    
+    data = 0
+    return data
+
+# 2: Data statistik funktion:
+def dataStatistics(data, statistic):
+    # Insert your code here
+    
+    result = 0
+    return result
+
+# 3: Data plot funktion:
+def dataPlot(data):
+    # Insert your code here
+    return
+    
+# 4: Hoved-script:
