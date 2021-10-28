163f8e5fb1937a8fa110aee0590906eebf571da2
Frederikravnborg <frederik@ravnborg.fr>
2021-10-28 15:48:59 +0200

    Update Main Script.py

--- Main Script.py
+++ Main Script.py
@@ -10,19 +10,26 @@ import panda as pd
 
 # 1: Data load Funktion:
 def dataLoad(filename):
-
+    # Importing the data as a matrix using the panda module
     data = pd.read_csv("test.txt", header=None, delimiter=' ', usecols=[0,1,2])
+    
+    # Calculating the number of rows in the matrix
     datanp = np.array(data)
     shape = np.shape(datanp)
     nr_rows = shape[0]
     
+    # Creating a for loop that goes through each row
     for i in range(nr_rows):
+        
+        # Removing rows with a temperature smaller than 10 or greater than 60
         if data.loc[i,0] < 10 or data.loc[i,0] > 60:
             data = data.drop(i,axis=0)
-            
+        
+        #Removing rows with negative growth rate    
         elif data.loc[i,1] <0:
             data = data.drop(i,axis=0)
         
+        #Removing rows with bacteria ID not matching either 1, 2, 3 or 4
         elif data.loc[i,2] not in [1,2,3,4]:
             data = data.drop(i,axis=0)
     return data
