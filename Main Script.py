2f355c4d8c163e527f53eeae65bc49f96cc047cd
Frederikravnborg <frederik@ravnborg.fr>
2021-10-28 16:17:47 +0200

    Update Main Script.py

--- Main Script.py
+++ Main Script.py
@@ -43,8 +43,31 @@ def dataStatistics(data, statistic):
 
 # 3: Data plot funktion:
 def dataPlot(data):
+        data = dataLoad("test.txt")
     
+    # Creating an array containing all the bacteria types
+    bacteria_type = np.array(data[2])
     
-    return
+    # Computing how many occurences each bacteria has in bacteria_type
+    b1 = np.sum(bacteria_type == 1)
+    b2 = np.sum(bacteria_type == 2)
+    b3 = np.sum(bacteria_type == 3)
+    b4 = np.sum(bacteria_type == 4)
+    
+    # Defining the x and y axis
+    x = ['1', '2', '3', '4']
+    y = np.array([b1,b2,b3,b4])
+    
+    # Designing the plot
+    plt.bar(x[0], y[0], color="g")
+    plt.bar(x[1], y[1], color="b")
+    plt.bar(x[2], y[2], color="y")
+    plt.bar(x[3], y[3], color="r")
+    plt.title("Number of each bacteria type")
+    plt.xlabel("Bacteria type")
+    plt.ylabel("Number of bacteria")
+    plt.xlim([-0.5, 3.5])
+    plt.ylim([0, np.max(y)+3])
+    plt.show()
     
 # 4: Hoved-script:
