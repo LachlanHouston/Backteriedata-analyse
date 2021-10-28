a8926f85c215343cf9711ddc31a722a216b20c0c
Frederikravnborg <frederik@ravnborg.fr>
2021-10-28 14:34:24 +0200

    Update Main Script.py

--- Main Script.py
+++ Main Script.py
@@ -10,9 +10,21 @@ import panda as pd
 
 # 1: Data load Funktion:
 def dataLoad(filename):
+
+    data = pd.read_csv("test.txt", header=None, delimiter=' ', usecols=[0,1,2])
+    datanp = np.array(data)
+    shape = np.shape(datanp)
+    nr_rows = shape[0]
     
-    
-    data = 0
+    for i in range(nr_rows):
+        if data.loc[i,0] < 10 or data.loc[i,0] > 60:
+            data = data.drop(i,axis=0)
+            
+        elif data.loc[i,1] <0:
+            data = data.drop(i,axis=0)
+        
+        elif data.loc[i,2] not in [1,2,3,4]:
+            data = data.drop(i,axis=0)
     return data
 
 # 2: Data statistik funktion:
