f345614004c946eb1d9993acfc00d0527ff45ff0
Frederikravnborg <frederik@ravnborg.fr>
2021-10-28 09:10:43 +0200

    Update Main Script.py

--- Main Script.py
+++ Main Script.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Oct 27 16:04:58 2021
 
-@author: Lachlan Houston(s214593) og Frederik Ravnborg(sxxxxxx)
+@author: Lachlan Houston(s214593) og Frederik Ravnborg(s204078)
 """
 import math
 import numpy as np
@@ -10,21 +10,22 @@ import panda as pd
 
 # 1: Data load Funktion:
 def dataLoad(filename):
-    # Insert your code here
+    
     
     data = 0
     return data
 
 # 2: Data statistik funktion:
 def dataStatistics(data, statistic):
-    # Insert your code here
+    
     
     result = 0
     return result
 
 # 3: Data plot funktion:
 def dataPlot(data):
-    # Insert your code here
+    
+    
     return
     
 # 4: Hoved-script:
