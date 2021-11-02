8b10a0fbbd58d8e75b12f07088f71064e0c982ce
Frederikravnborg <frederik@ravnborg.fr>
2021-11-02 20:02:04 +0100

    Update Main Script.py

--- Main Script.py
+++ Main Script.py
@@ -40,81 +40,103 @@ def dataStatistics(data, statistic):
     return result
 
 # 3: Data plot funktion:
-#def dataPlot(data):
-data = dataLoad("test.txt")
-
-# Creating an array containing all the bacteria types
-bacteria_type = np.array(data[2])
-
-# Computing how many occurences each bacteria has in bacteria_type
-b1 = np.sum(bacteria_type == 1)
-b2 = np.sum(bacteria_type == 2)
-b3 = np.sum(bacteria_type == 3)
-b4 = np.sum(bacteria_type == 4)
-
-# Defining the x and y axis
-x = ['1', '2', '3', '4']
-y = np.array([b1,b2,b3,b4])
-
-# Designing the plot
-plt.bar(x[0], y[0], color="g")
-plt.bar(x[1], y[1], color="b")
-plt.bar(x[2], y[2], color="y")
-plt.bar(x[3], y[3], color="r")
-plt.title("Number of each bacteria type")
-plt.xlabel("Bacteria type")
-plt.ylabel("Number of bacteria")
-plt.xlim([-0.5, 3.5])
-plt.ylim([0, np.max(y)+3])
-plt.show()
+def dataPlot(data):
+    data = dataLoad("test.txt")
     
-  
-# Del, der ikke virker:
-data = dataLoad("test.txt")
-datanp = np.array(data)
-shape = np.shape(datanp)
-nr_rows = shape[0]
-x1 = np.zeros(b1)
-x2 = np.zeros(b2)
-x3 = np.zeros(b3)
-x4 = np.zeros(b4)
-y1 = np.zeros(b1)
-y2 = np.zeros(b2)
-y3 = np.zeros(b3)
-y4 = np.zeros(b4)
+    # Creating an array containing all the bacteria types
+    bacteria_type = np.array(data[2])
+    
+    # Computing how many occurences each bacteria has in bacteria_type
+    b1 = np.sum(bacteria_type == 1)
+    b2 = np.sum(bacteria_type == 2)
+    b3 = np.sum(bacteria_type == 3)
+    b4 = np.sum(bacteria_type == 4)
+    
+    # Defining the x and y axis
+    x = ['1', '2', '3', '4']
+    y = np.array([b1,b2,b3,b4])
+    
+    # Designing the plot
+    plt.bar(x[0], y[0], color="g")
+    plt.bar(x[1], y[1], color="b")
+    plt.bar(x[2], y[2], color="C1")
+    plt.bar(x[3], y[3], color="r")
+    plt.title("Number of each bacteria type")
+    plt.xlabel("Bacteria type")
+    plt.ylabel("Number of bacteria")
+    plt.xlim([-0.5, 3.5])
+    plt.ylim([0, np.max(y)+3])
+    plt.show()
+        
+      
+    data = dataLoad("test.txt")
+    datanp = np.array(data)
+    shape = np.shape(datanp)
+    nr_rows = shape[0]
+    x1 = np.zeros(b1)
+    x2 = np.zeros(b2)
+    x3 = np.zeros(b3)
+    x4 = np.zeros(b4)
+    y1 = np.zeros(b1)
+    y2 = np.zeros(b2)
+    y3 = np.zeros(b3)
+    y4 = np.zeros(b4)
+    
+    x = 0
+    y = 0
+    w = 0
+    z = 0
+    
+    
+    for i in range(shape[0]):
+        if datanp[i,2] == 1:
+            x1[y] = datanp[i,0]
+            y1[y] = datanp[i,1]
+            y += 1
+        
+        if datanp[i,2] == 2:
+            x2[x] = datanp[i,0]
+            y2[x] = datanp[i,1]
+            x += 1
+            
+        if datanp[i,2] == 3:
+            x3[w] = datanp[i,0]
+            y3[w] = datanp[i,1]
+            w += 1
+            
+        if datanp[i,2] == 4:
+            x4[z] = datanp[i,0]
+            y4[z] = datanp[i,1]
+            z += 1
+    
+    
+    sort1 = x1.argsort()
+    x1 = x1[sort1]
+    y1 = y1[sort1]
+    
+    sort2 = x2.argsort()
+    x2 = x2[sort2]
+    y2 = y2[sort2]
+    
+    sort3 = x3.argsort()
+    x3 = x3[sort3]
+    y3 = y3[sort3]
+    
+    sort4 = x4.argsort()
+    x4 = x4[sort4]
+    y4 = y4[sort4]
+    
+    
+    plt.plot(x1, y1, color="g")
+    plt.plot(x2, y2, color="b")
+    plt.plot(x3, y3, color="C1")
+    plt.plot(x4, y4, color="r")
+    plt.title("Growth rate by temperature")
+    plt.xlabel("Temperature")
+    plt.ylabel("Growth rate")
+    plt.xlim([0, 60])
+    plt.legend(["Bacteria 1", "Bacteria 2", "Bacteria 3", "Bacteria 4"], loc ="upper left")
+    plt.show()
 
-x = 0
-y = 0
-w = 0
-z = 0
 
-
-for i in range(shape[0]):
-    if datanp[i,2] == 2:
-        x2[x] = datanp[i,0]
-        y2[x] = datanp[i,1]
-        x += 1
-        
-    if datanp[i,2] == 1:
-        x1[y] = datanp[i,0]
-        y1[y] = datanp[i,1]
-        y += 1
-        
-    if datanp[i,2] == 3:
-        x3[w] = datanp[i,0]
-        y3[w] = datanp[i,1]
-        w += 1
-        
-    if datanp[i,2] == 4:
-        x4[z] = datanp[i,0]
-        y4[z] = datanp[i,1]
-        z += 1
-        
-print(x1,y1)
-print(" ")
-print(x2,y2)
-print(" ")
-print(x3,y3)
-print(" ")
-print(x4,y4)
-print(" ")
+print(dataPlot())
